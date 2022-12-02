08cd841b4d041c617bf42afc3ef5132290f781b7
Claiyc <claiyc@outlook.com>
2022-12-02 13:14:21 +0100

    approximate matching experiments

--- src/proc/Vigad.ts
+++ src/proc/Vigad.ts
@@ -1,10 +1,35 @@
 import RandExp from "randexp";
+var stringSimilarity = require("string-similarity");
 
 export class Vigad {
     public main() {
-        let data:string = "Here we have some random text that was HP: 55 fetched from the image, including som data in between."
+        let data:string = "Here we have some random text that was HP: 55 fetched fromPlayer: Marc the image, including som data in between."
+        let dataSubstr = this.getAllSubstrings(data);
+        console.log(dataSubstr);
 
-        this.match(data, /[0-100]/); // value regex
+        let firstRegex: RegExp = /Player\:/;
+        let firstRegexMatches = this.genMatches(firstRegex, 50000);
+        let highestRating = 0;
+        let highestRatingElem = "";
+        dataSubstr.forEach(element => {
+            let bestMatch = this.bestMatch(element, firstRegexMatches);
+            if (bestMatch.rating > highestRating) {
+                highestRating = bestMatch.rating;
+                highestRatingElem = element;
+            } else if (bestMatch.rating == highestRating && element.length > highestRatingElem.length) { // accept longest element rather
+                highestRatingElem = element;
+            }
+            if (highestRating == 1) {
+                return; // does not work with foreach but can be a great performance optimizer
+            }
+        });
+        console.log("Best Match first Regex: ");
+        console.log(highestRatingElem);
+
+        let valueRegex = /^\w{1,10}\b/;
+
+        //this.match(data, regex);
+        //console.log(this.bestMatch(data, regex));
     }
 
 
@@ -18,6 +43,8 @@ export class Vigad {
         console.log(res);
         console.log("---------------------------------------------");
         console.log(this.matchingToPercentage(data, regex));
+        console.log("----------------------bestMatch-----------------------");
+        //console.log(this.bestMatch(valuesubstr, regex));
     }
 
     private indexOfFirst(data:string, regex:RegExp, before:boolean = false):number {
@@ -33,5 +60,38 @@ export class Vigad {
         const randexp = new RandExp(regex);
         console.log(randexp.gen());
     }
+
+    private genMatches(regex:RegExp, max:number) {
+        let regLev0: string[] = [];
+        let randexp = new RandExp(regex);
+        for (let i = 0; i < max; i++) {
+            let exp = randexp.gen();
+            if (!regLev0.includes(exp)) {
+                regLev0.push(exp);
+            }
+        }
+        console.log("generated");
+        return regLev0;
+    }
+
+    private bestMatch(data:string, genMatches:string[]) {
+        let matches = stringSimilarity.findBestMatch(data, genMatches);
+        //console.log("Matches:");
+        //console.log(matches);
+        console.log("Best Match to '" + data + "':");
+        console.log(matches.bestMatch);
+        return matches.bestMatch;
+    }
+
+    private getAllSubstrings(str:string) {
+        var i, j, result = [];
+      
+        for (i = 0; i < str.length; i++) {
+            for (j = i + 1; j < str.length + 1; j++) {
+                result.push(str.slice(i, j));
+            }
+        }
+        return result;
+      }
 
 }

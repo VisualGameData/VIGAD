e5e1445730276a1adca6a01bfad290ec36103de2
Kevin Beier <beierkevin2002@outlook.com>
2022-12-09 00:30:04 +0100

    reimplementation of the sources tabs
    Signed-off-by: Kevin Beier <beierkevin2002@outlook.com>

--- src/views/SourcesView.vue
+++ src/views/SourcesView.vue
@@ -4,37 +4,107 @@
         subtitle="Show you all the available screen and application sources"
         :loading="isLoadingScreensAndWindows"
     >
-        <template v-slot:actions>
-            <v-btn
-                @click="fetchAllStreams()"
-                color="primary"
-                width="100%"
-                prepend-icon="mdi-refresh"
-                variant="tonal"
-                >Refresh</v-btn
-            >
-        </template>
         <template v-slot:default>
+            <v-tabs v-model="tabs" class="mb-4" color="primary" grow>
+                <v-tooltip location="top" text="All screens">
+                    <template v-slot:activator="{ props }">
+                        <v-tab v-bind="props" value="screens">
+                            <v-icon>mdi-monitor</v-icon>
+                        </v-tab>
+                    </template>
+                </v-tooltip>
+
+                <v-tooltip location="top" text="All applications">
+                    <template v-slot:activator="{ props }">
+                        <v-tab v-bind="props" value="applications">
+                            <v-icon>mdi-application</v-icon>
+                        </v-tab>
+                    </template>
+                </v-tooltip>
+            </v-tabs>
+
             <div v-if="!isLoadingScreensAndWindows" class="windows-wrapper">
-                <v-card
-                    v-for="(source, index) in desktopVideoStreamSources"
-                    :key="source.id"
-                    @click="
-                        streamHandler.setCurrentSelectedSource(streams[index])
-                    "
-                    variant="tonal"
-                    class="mb-2"
-                >
-                    <v-card-title>{{ source.name }}</v-card-title>
-                    <v-card-text>
-                        <video
-                            autoplay
-                            :src-object.prop.camel="streams[index]"
-                            class="preview"
-                        ></video>
-                    </v-card-text>
-                </v-card>
+                <v-window v-model="tabs">
+                    <v-window-item value="screens">
+                        <v-btn
+                            class="mb-4"
+                            variant="plain"
+                            width="100%"
+                            prepend-icon="mdi-refresh"
+                            @click="fetchAllStreams()"
+                        >
+                            Refresh
+                        </v-btn>
+                        <div
+                            v-if="!isLoadingScreensAndWindows"
+                            class="windows-wrapper"
+                        >
+                            <v-card
+                                v-for="(source, index) in onlyScreenSources"
+                                :key="source.id"
+                                @click="
+                                    streamHandler.setCurrentSelectedSource(
+                                        streams[index]
+                                    )
+                                "
+                                variant="tonal"
+                                class="mb-2"
+                            >
+                                <v-card-title>{{ source.name }}</v-card-title>
+                                <v-card-text>
+                                    <video
+                                        autoplay
+                                        :src-object.prop.camel="streams[index]"
+                                        class="preview"
+                                    ></video>
+                                </v-card-text>
+                            </v-card>
+                        </div>
+                    </v-window-item>
+
+                    <v-window-item value="applications">
+                        <v-btn
+                            class="mb-4"
+                            variant="plain"
+                            width="100%"
+                            prepend-icon="mdi-refresh"
+                            @click="fetchAllStreams()"
+                        >
+                            Refresh
+                        </v-btn>
+                        <div
+                            v-if="!isLoadingScreensAndWindows"
+                            class="windows-wrapper"
+                        >
+                            <v-card
+                                v-for="(
+                                    source, index
+                                ) in onlyApplicationSources"
+                                :key="source.id"
+                                @click="
+                                    streamHandler.setCurrentSelectedSource(
+                                        streams[lastScreenIndex + index]
+                                    )
+                                "
+                                variant="tonal"
+                                class="mb-2"
+                            >
+                                <v-card-title>{{ source.name }}</v-card-title>
+                                <v-card-text>
+                                    <video
+                                        autoplay
+                                        :src-object.prop.camel="
+                                            streams[lastScreenIndex + index]
+                                        "
+                                        class="preview"
+                                    ></video>
+                                </v-card-text>
+                            </v-card>
+                        </div>
+                    </v-window-item>
+                </v-window>
             </div>
+            <div v-else>Loading ...</div>
         </template>
     </ViewComponent>
 </template>
@@ -51,10 +121,16 @@ const vigad = ref(Vigad.getInstance());
 const streamHandler = vigad.value.getStreamHandlerInstance();
 
 // For the Screen / Application Tab
-const tab = ref(null);
+const tabs = ref(null);
 
 // Get all desktop video stream sources
 const desktopVideoStreamSources = ref();
+
+const onlyApplicationSources = ref();
+const onlyScreenSources = ref();
+const lastScreenIndex = computed(() => {
+    return Object.keys(onlyScreenSources.value).length;
+});
 
 // All MediaStream sources
 const streams = ref<MediaStream[]>([]);
@@ -72,9 +148,20 @@ onMounted(() => {
 async function fetchAllStreams() {
     isLoadingScreensAndWindows.value = true;
 
+    // Reset all sources
+    desktopVideoStreamSources.value = null;
+    onlyApplicationSources.value = null;
+    onlyScreenSources.value = null;
+
     // wait for the fetched screens and windows
     desktopVideoStreamSources.value =
         await streamHandler.getScreenAndApplicationSources();
+
+    // TODO: testing
+    onlyApplicationSources.value =
+        await streamHandler.getOnlyApplicationSources();
+
+    onlyScreenSources.value = await streamHandler.getOnlyScreenSources();
 
     // Loop through all sources and set the MediaStream to video nodes
     Array.from(desktopVideoStreamSources.value).forEach(function (element) {

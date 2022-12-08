31dd82275353e5b988014e8e3144e68b30bdf897
Kevin Beier <beierkevin2002@outlook.com>
2022-12-09 00:30:55 +0100

    initial try for reimplementation
    Signed-off-by: Kevin Beier <beierkevin2002@outlook.com>

--- src/components/capture-area/CaptureAreaSearchValue.vue
+++ src/components/capture-area/CaptureAreaSearchValue.vue
@@ -5,22 +5,7 @@
         class="mb-4"
         prepend-icon="mdi-magnify"
     >
-        <template v-slot:title>
-            <v-row no-gutter>
-                <v-col>Find Value</v-col>
-                <v-spacer />
-                <v-col>
-                    <v-btn
-                        color="green"
-                        prepend-icon="mdi-plus"
-                        size="small"
-                        rounded="lg"
-                        variant="tonal"
-                        >Enable</v-btn
-                    ></v-col
-                >
-            </v-row>
-        </template>
+        <template v-slot:title> Find Value </template>
 
         <v-divider color="primary" class="mb-4"></v-divider>
 
@@ -40,70 +25,49 @@
                 ></v-col>
             </v-row>
 
-            <v-row class="mb-4" no-gutters cols="12">
-                <v-col cols="2">is</v-col>
-                <v-col cols="3"
-                    ><v-combobox
-                        class="pr-2 pl-2"
-                        label="Position"
-                        :items="['After', 'Before', 'Between']"
-                        variant="underlined"
-                    ></v-combobox
-                ></v-col>
-                <v-col cols="7"
-                    ><v-text-field
-                        cols="1"
-                        label="Regular Expression"
-                        placeholder="Enter a regular expression"
-                        variant="outlined"
-                        dense
-                        clear-icon="mdi-close-circle"
-                        :rules="[
-                            (v) => !!v || 'Regular expression is required',
-                        ]"
-                    ></v-text-field
-                ></v-col>
+            <v-row class="mb-4" no-gutter>
+                <v-combobox
+                    label="Slicing Options"
+                    :items="['All substrings', 'At spaces', 'Entire string']"
+                    variant="underlined"
+                ></v-combobox>
             </v-row>
 
-            <v-row class="mb-4" no-gutters>
-                <v-col cols="2"
-                    ><v-combobox
-                        label="Logic"
-                        :items="['AND', 'OR', 'NOT']"
-                        variant="underlined"
-                    ></v-combobox
-                ></v-col>
-                <v-col cols="3"
-                    ><v-combobox
-                        class="pr-2 pl-2"
-                        label="Position"
-                        :items="['After', 'Before', 'Between']"
-                        variant="underlined"
-                    ></v-combobox
-                ></v-col>
-                <v-col cols="7"
-                    ><v-text-field
-                        label="Regular Expression"
-                        placeholder="Enter a regular expression"
-                        variant="outlined"
-                        dense
-                        clear-icon="mdi-close-circle"
-                        :rules="[
-                            (v) => !!v || 'Regular expression is required',
-                        ]"
-                    ></v-text-field
-                ></v-col>
-                <!-- <v-col
-          ><v-btn
-            color="red"
-            icon="mdi-delete"
-            rounded="lg"
-            variant="tonal"
-          ></v-btn
-        ></v-col> -->
+            <v-row class="mb-4" no-gutter>
+                <v-combobox
+                    label="Matching"
+                    :items="[
+                        'Approximate (default - constraint regex)',
+                        'Exact (default - value regex)',
+                    ]"
+                    variant="underlined"
+                ></v-combobox>
             </v-row>
 
-            <v-row class="mb-4" no-gutters align="center">
+            <v-row class="mb-4" no-gutter>
+                <v-text-field
+                    label="Regular Expression"
+                    placeholder="Amount of generated matches"
+                    variant="outlined"
+                    dense
+                    clear-icon="mdi-close-circle"
+                    :rules="[(v) => !!v || 'Regular expression is required']"
+                ></v-text-field>
+            </v-row>
+
+            <v-row class="mb-4" no-gutter>
+                <v-combobox
+                    label="Similarity conversions"
+                    :items="[
+                        'None',
+                        'Numbers to Letters',
+                        'Letters to Numbers',
+                    ]"
+                    variant="underlined"
+                ></v-combobox>
+            </v-row>
+
+            <!-- <v-row class="mb-4" no-gutters align="center">
                 <v-col>
                     <v-divider color="grey" />
                 </v-col>
@@ -120,10 +84,9 @@
                 <v-col>
                     <v-divider color="grey" />
                 </v-col>
-            </v-row>
+            </v-row> -->
         </v-card-text>
         <v-card-actions>
-            <!-- TODO: proper layouting is missing -->
             <v-row no-gutter>
                 <v-col>
                     <v-btn

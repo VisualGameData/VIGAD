5836aebd1a71e3824289251ce7158e6c05ccc79b
Kevin Beier <beierkevin2002@outlook.com>
2022-12-08 23:18:45 +0100

    removed unused import
    Signed-off-by: Kevin Beier <beierkevin2002@outlook.com>

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
 import vuetify from './plugins/vuetify';
-import VueDragResize from 'vue3-drag-resize';
 // https://github.com/nikitasnv/vue-resizable
 
 createApp(App)
-    .component('vue-drag-resize', VueDragResize)
     .use(vuetify)
     .use(router)
     .mount('#app')

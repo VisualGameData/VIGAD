9e70b778aeafac3e2e60cd85ec1db281cea109f2
Claiyc <claiyc@outlook.com>
2022-12-11 16:37:29 +0100

    first part of implementation

--- src/proc/TesseractHandler.ts
+++ src/proc/TesseractHandler.ts
@@ -1,8 +1,90 @@
-import { createWorker } from 'tesseract.js';
+import { createWorker, createScheduler, Worker } from 'tesseract.js';
 import { CaptureArea } from './CaptureArea';
 
 export class TesseractHandler {
-    private stream?: MediaStream;
-    private worker: Worker[] = [];
-    private enabledCaptureAreas: CaptureArea[] = [];
+    private static instance: TesseractHandler;
+    private worker: Worker[];
+    private enabledCaptureAreas: CaptureArea[];
+
+    /**
+     * Create a private constructor to prevent multiple instances
+     */
+    private constructor() {
+        this.worker = [];
+        this.enabledCaptureAreas = [];
+    }
+
+    /**
+     * This function gets the singelton instance of TesseractHandler.ts
+     *
+     * @return TesseractHandler
+     */
+    public static getInstance(): TesseractHandler {
+        if (!this.instance) {
+            this.instance = new this();
+        }
+
+        return this.instance;
+    }
+
+    public async enableCaptureArea(ca: CaptureArea): Promise<void> {
+        this.enabledCaptureAreas.push(ca);
+        await this.addWorker();
+    }
+
+    public async addWorker(): Promise<void> {
+        const worker = createWorker({cachePath: "."});
+        await worker.load();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+        this.worker.push(worker);
+        console.log("added tesseract worker");
+    }
+
+    public removeWorker(): void {
+        this.worker.pop();
+    }
+
+    public async run(stream:MediaStream): Promise<void> {
+        while (true) {
+            await new Promise(async (resolve) => {
+                const scheduler = createScheduler();
+                this.worker.forEach((worker) => {
+                    scheduler.addWorker(worker);
+                });
+
+                const video = document.createElement('video');
+                video.srcObject = stream;
+                video.play();
+
+                // on first frame loaded
+                video.addEventListener('loadeddata', () => {
+                    // generate image from video
+                    const canvas = document.createElement('canvas');
+                    canvas.height = video.videoHeight;
+                    canvas.width = video.videoWidth;
+                    canvas.getContext('2d')!.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+                    const img = canvas.toDataURL('image/png');
+                    video.pause();
+
+                    // create rectangles from capture areas
+                    const rectangles = this.enabledCaptureAreas.map((ca) => {
+                        return {
+                            left: ca.getLeft(),
+                            top: ca.getTop(),
+                            width: ca.getWidth(),
+                            height: ca.getHeight(),
+                        };
+                    });
+
+                    // run tesseract
+                    (async () => {
+                        const results = await Promise.all(rectangles.map((rectangle) => (
+                            scheduler.addJob('recognize', img, {rectangle}).then((x) => console.log(x.data.text))
+                        )));
+                    })();
+                });
+            });
+        }
+    }
 }
--- src/proc/Vigad.ts
+++ src/proc/Vigad.ts
@@ -1,9 +1,11 @@
 import { CaptureArea } from './CaptureArea';
 import { StreamHandler } from './StreamHandler';
+import { TesseractHandler } from './TesseractHandler';
 
 export class Vigad {
     private static instance: Vigad;
     private streamHandler: StreamHandler;
+    private tesseractHandler: TesseractHandler;
     private captureAreas: CaptureArea[];
 
     /**
@@ -11,6 +13,7 @@ export class Vigad {
      */
     private constructor() {
         this.streamHandler = StreamHandler.getInstance();
+        this.tesseractHandler = TesseractHandler.getInstance();
         this.captureAreas = [];
     }
 
@@ -48,6 +51,9 @@ export class Vigad {
         let ca = new CaptureArea(width, height, top, left);
         this.captureAreas.push(ca);
         ca.setId(this.captureAreas.length - 1);
+        this.tesseractHandler.enableCaptureArea(ca).then(() => {
+            this.startTesseract();
+        });
         return ca.getId();
     }
 
@@ -58,6 +64,7 @@ export class Vigad {
      */
     public deleteCaptureArea(id: number): void {
         this.captureAreas.splice(id, 1);
+        this.tesseractHandler.removeWorker();
     }
 
     /**
@@ -75,5 +82,14 @@ export class Vigad {
      */
     public getAllCaptureAreas(): CaptureArea[] {
         return this.captureAreas;
+    }
+
+    public startTesseract(): void {
+        this.tesseractHandler.run(this.streamHandler.getCurrentSelectedSource());
+        console.log("started tesseract");
+    }
+
+    public stopTesseract(): void {
+
     }
 }

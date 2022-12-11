c2ca279f411b1e289e9d12741cb6381f9e6db8a3
Kevin Beier <beierkevin2002@outlook.com>
2022-12-11 16:31:29 +0100

    extracted loging output into component
    Signed-off-by: Kevin Beier <beierkevin2002@outlook.com>

---
+++ src/components/LogOutput.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-alert
+        border="start"
+        icon="mdi-information"
+        :title="title"
+        type="success"
+        class="log-output-container"
+    >
+        {{ log }}
+    </v-alert>
+</template>
+
+<script setup lang="ts">
+import { type } from 'os';
+
+const props = defineProps<{
+    title: string;
+    log: string;
+}>();
+</script>
+
+<style lang="scss" scoped>
+.log-output-container {
+    max-height: 350px;
+    overflow-y: auto;
+}
+</style>
--- src/views/RunPage.vue
+++ src/views/RunPage.vue
@@ -19,14 +19,10 @@
             >
         </template>
         <template v-slot:default>
-            <v-alert
-                border="start"
-                icon="mdi-information"
+            <LogOutput
                 title="Capture Area 1"
-                type="success"
-            >
-                Log: <br />
-            </v-alert>
+                log="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+            />
         </template>
     </ViewComponent>
 </template>
@@ -34,6 +30,7 @@
 <script setup lang="ts">
 import ViewComponent from '@/components/ViewComponent.vue';
 import { useRunning, isRunning } from '@/composables/useRunning';
+import LogOutput from '@/components/LogOutput.vue';
 </script>
 
 <style lang="scss" scoped></style>

4bdbda5d26602a989ec4dc18f95e8d9256708cc9
Kevin Beier <beierkevin2002@outlook.com>
2023-04-17 19:18:59 +0200

    removed unneccessary component tests

--- src/components/ViewComponent/ViewComponent.test.ts
+++ src/components/ViewComponent/ViewComponent.test.ts
@@ -1,16 +1,28 @@
 // @ts-nocheck
 import { describe, it, expect } from 'vitest';
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import ViewComponent from '@/components/ViewComponent/ViewComponent.vue';
 
 describe('ViewComponent', () => {
-    it('renders the title prop', () => {
-        const title = 'Test Title';
-        const wrapper = mount(ViewComponent, {
-            props: {
-                title,
-            },
-        });
-        expect(wrapper.find('.header-container').text()).toContain(title);
-    });
-});
+  it('renders the title prop', () => {
+    const title = 'Test Title'
+    const wrapper = shallowMount(ViewComponent, {
+      props: { title }
+    })
+    expect(wrapper.find('.header-container').text()).toContain(title)
+  })
+
+  it('renders default slot content', () => {
+    const wrapper = shallowMount(ViewComponent, {
+      slots: { default: '<div class="my-slot-content"></div>' }
+    })
+    expect(wrapper.find('.my-slot-content').exists()).toBe(true)
+  })
+
+  it('renders actions slot content', () => {
+    const wrapper = shallowMount(ViewComponent, {
+      slots: { actions: '<div class="my-actions-content"></div>' }
+    })
+    expect(wrapper.find('.my-actions-content').exists()).toBe(true)
+  })
+})

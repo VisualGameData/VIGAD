d361432e4e2c6e86c06206532a9fe63262558aab
Kevin Beier <beierkevin2002@outlook.com>
2023-04-18 09:11:40 +0200

    removed ignore ts checking line

--- src/components/ViewComponent/ViewComponent.test.ts
+++ src/components/ViewComponent/ViewComponent.test.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { describe, it, expect } from 'vitest';
 import { shallowMount } from '@vue/test-utils';
 import ViewComponent from '@/components/ViewComponent/ViewComponent.vue';
@@ -7,7 +6,11 @@ describe('ViewComponent', () => {
   it('renders the title prop', () => {
     const title = 'Test Title'
     const wrapper = shallowMount(ViewComponent, {
-      props: { title }
+      props: 
+      { 
+        title: title,
+        loading: false,
+      }
     })
     expect(wrapper.find('.header-container').text()).toContain(title)
   })

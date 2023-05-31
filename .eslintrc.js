716abed1d8e8dff032f4d506ec327b263b89f9f5
Kevin Beier <beierkevin2002@outlook.com>
2023-05-31 14:58:17 +0200

    updating eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,33 @@
 module.exports = {
+    root: true,
     env: {
+        node: true,
         browser: true,
         es2021: true,
     },
+    plugins: ['vue', 'vuetify', '@typescript-eslint', 'prettier'],
     extends: [
+        'plugin:vue/vue3-recommended',
+        'plugin:vue/base',
+        'plugin:vuetify/base',
+        'plugin:vuetify/recommended',
         'eslint:recommended',
-        'plugin:vue/essential',
         'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'prettier',
     ],
+    parser: 'vue-eslint-parser',
     parserOptions: {
         ecmaVersion: 'latest',
         parser: '@typescript-eslint/parser',
         sourceType: 'module',
     },
-    plugins: ['vue', '@typescript-eslint'],
     rules: {
-        // override/add rules settings here, such as:
         'vue/no-unused-vars': 'error',
         'vue/script-setup-uses-vars': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        'prettier/prettier': 'error',
+        'grid-unknown-attributes': 'off',
     },
 };
